227beb6cb0c036cf5b2e6cfba419955cffdf9c10
Максим Иванов <vova36kozy@gmail.com>
2023-03-19 13:28:35 +0700

    I don't know

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const TelegramApi = require('node-telegram-bot-api')
-
 const token = "6069489445:AAFz1VrhNA7W2lT04FZUv0334GemK__39Rs"
-
 const bot = new TelegramApi(token, {polling: true})
 
 bot.on("message", msg => {
     const text = msg.text
     const chatId = msg.chat.id
-    // if(text === "/start") {
-    //     await bot.sendMessage(chatId, `Введите пароль`)
-    // }
-    bot.sendMessage(chatId, `123`)
+
+    if(text === "/start") {
+        bot.sendMessage(chatId, `Добро пожаловать ${msg.chat.username}!`)
+    }
+
+    //bot.sendMessage(chatId, getNowDate())
 })
+
+function getNowDate() {
+    let now = new Date().getDate() + "."
+    let month = new Date().getMonth() + 1
+    if(month < 10) {
+        now += "0"
+    }
+    return now += month
+}
+
+// if(text === "/start") {
+//   await bot.sendMessage(chatId, `Введите пароль`)
+// }
